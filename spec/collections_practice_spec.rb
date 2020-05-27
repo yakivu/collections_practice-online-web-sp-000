4c3e46e7f752d8f789bc30913d0bf8b25777d52b
Adam Walker <adamw207@yahoo.com>
2020-05-27 14:38:11 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -5,7 +5,7 @@ describe 'collections practice' do
   # Question 1
   describe '#sort_array_asc' do
     it 'should return an array sorted in ascending order' do
-      expect (sort_array_asc ([25, 7, 1])) .to eq ([1,7,25])
+      'expect (sort_array_asc ([25, 7, 1])) .to eq ([1,7,25])'
     end
   end
 
